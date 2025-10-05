ea02550bcf799887fa8133c305a18f2be666c01e
Radhika Sharma <radhikabhardwaj16903@gmail.com>
2025-10-05 22:48:32 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ connectDB();
 // Middlewares
 app.use(helmet()); // security headers
 app.use(cors({
-  origin: process.env.CLIENT_URL, // frontend URL, can update later
+  origin: 'https://microcourses-frontend-4azj.onrender.com, // frontend URL, can update later
   credentials: true,               // allow cookies
 }));
 app.use(express.json());  // parse JSON request bodies
@@ -44,3 +44,4 @@ app.use((err, req, res, next) => {
 });
 
 module.exports = app;
+
